d11c9bd5c0664ef5bee0703df282c6a65d09b0d7
Jojo Thomas <jojothomas1515@gmail.com>
2023-12-18 05:54:31 +0100

    bug fix

--- setup_nginx.sh
+++ setup_nginx.sh
@@ -1,5 +1,5 @@
-sudo cp myportfolio_react.conf /etc/nginx/sites-available/
+cp myportfolio_react.conf /etc/nginx/sites-available/
 
-sudo ln -sf /etc/nginx/sites-available/myportfolio_react.conf /etc/nginx/sites-enabled/myportfolio_react.conf
+ln -sf /etc/nginx/sites-available/myportfolio_react.conf /etc/nginx/sites-enabled/myportfolio_react.conf
 
-sudo service nginx restart
+service nginx restart
