f18af13ea75b2ee18f4a97e6f795f480603157d0
Jojo Thomas <jojothomas1515@gmail.com>
2023-12-22 18:01:23 +0100

    footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; 2023 Joseph Thomas Ehigie</p>
+
+      <div className='links'>
+        <a href='https://www.github.com/jojothomas1515'>Github</a>
+        <a href='https://www.linkedin.com/in/joseph-thomas-ehigie-551452210'>LinkedIn</a>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
